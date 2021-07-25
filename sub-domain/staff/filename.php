d563f7cffe3dca933606dbe634261317f0b4adff
Monster2408 <meoto2408@gmail.com>
2021-07-25 22:32:27 +0900

    .gitignore更新(サブドメイン関連)

---
+++ sub-domain/staff/filename.php
@@ -0,0 +1,52 @@
+<?php
+
+$config = include($_SERVER["DOCUMENT_ROOT"] . '/assets/config.php');
+
+$TITLE = "動画ファイル雛形 | MonsterLifeServer";
+$URL = $conf["url"] . '/';
+$DESCRIPTION = "テンプレート。テンプレート。テンプレート。";
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+	<head>
+        <?php echo $html["common_head"]; ?>
+		<title><?php echo $TITLE; ?></title>
+		<meta property="og:url" content="<?php echo $URL; ?>/" />
+		<meta property="og:title" content="<?php echo $TITLE; ?>" />
+		<meta property="og:description" content="<?php echo $DESCRIPTION; ?>" />
+		<link rel="stylesheet" href="<?php echo $conf["url"]; ?>/assets/css/carousel.min.css" type="text/css">
+		<script type="text/javascript" src="<?php echo $conf["url"]; ?>/assets/js/rss_reader.js"></script>
+    </head>
+    <body>
+        <?php include( $_SERVER["DOCUMENT_ROOT"] . "/assets/include/header.php"); ?>
+        <div class="wrapper">
+            <div class="mainBox">
+                <div class="contents">
+                    <!-- パンくずリスト -->
+                    <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+                        <li itemprop="itemListElement" itemscope
+                            itemtype="https://schema.org/ListItem">
+                            <a itemprop="item" href="https://staff.mlserver.xyz/">
+                                <span itemprop="name">ホーム</span>
+                            </a>
+                            <meta itemprop="position" content="1" />
+                        </li>
+
+                        <li itemprop="itemListElement" itemscope
+                            itemtype="https://schema.org/ListItem">
+                            <a itemprop="item" href="https://staff.mlserver.xyz/filename">
+                                <span itemprop="name">動画ファイル雛形</span>
+                            </a>
+                            <meta itemprop="position" content="2" />
+                        </li>
+                    </ol>
+                    <!-- パンくずリスト -->
+
+                </div>
+            </div>
+        </div>
+        <?php include( $_SERVER["DOCUMENT_ROOT"] . "/assets/include/footer.php"); ?>
+    </body>
+    <?php echo $html["common_foot"]; ?>
+</html>
