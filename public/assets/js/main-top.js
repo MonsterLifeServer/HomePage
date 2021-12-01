92f5ac146411d75455f19630d8692589bbacb85f
Monster2408 <meoto2408@gmail.com>
2021-12-01 23:25:51 +0900

    スタイルの修正

--- public/assets/js/main-top.js
+++ public/assets/js/main-top.js
@@ -1,7 +1,3 @@
-$(function() {
-    var height=$("#header").height();
-    $("body").css("margin-top", height + 10);//10pxだけ余裕をもたせる
-});
 
 $(window).on('load', function(){
     $('#loading').fadeOut();
