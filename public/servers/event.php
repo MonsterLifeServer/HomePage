50d06e1a8e469455c31fab456e9902ea607b95d3
Monster2408 <meoto2408@gmail.com>
2021-12-08 23:26:54 +0900

    thinking new design

--- public/servers/event.php
+++ public/servers/event.php
@@ -65,9 +65,17 @@ $DESCRIPTION = "ミニゲーム企画を開催するサーバーについて";
 					<h2 class="design">できること</h2>
                     <div class="box text-left">
 						<ul>
-							<li>1.12.2でミニゲームが遊ぶことができる。</li>
+                            <li>PCゲーム『青鬼』の館をマイクラで再現した鬼ごっこ型のミニゲームやスマホゲーム『青鬼ONLINE』をマイクラで再現したミニゲーム，増え鬼などあらゆるミニゲーム企画を開催・現在進行形で開発しております。当鯖に参加して遊べるミニゲームは<a href="<?php echo $conf["url"]; ?>/game/" target="_blank">ミニゲーム一覧</a>より確認できます。</li>
+                            <li>1.12.2でミニゲームが遊ぶことができる。<br>※他バージョンの場合は開催時に連絡いたします。</li>
 						</ul>
 					</div>
+
+                    <div class="youtube-box">
+                        <a href="https://www.youtube.com/watch?v=hZvyzitiUOo&list=PL_0GzODUwYMA-BibQjLkQAFW4otbFgSE3&index=5" target="_blank">
+                            <img src="https://i.gyazo.com/bdd7d051f9aa14a324306aba76f684ad.png" alt="" />
+                        </a>
+					</div>
+
 
 					<h2 class="design">禁止事項</h2>
 					<div class="box text-left">
@@ -78,7 +86,6 @@ $DESCRIPTION = "ミニゲーム企画を開催するサーバーについて";
                             <li>企画ごとに細かいルールが存在します。詳細は<a href="<?php echo $conf["url"]; ?>/game/">ミニゲーム企画一覧</a>からご確認ください。</li>
 						</ul>
 					</div>
-
                 </div>
             </div>
 
