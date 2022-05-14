7bc61d53cf6e9e7a552559b9c893ba7132398e14
Monster2408 <meoto2408@gmail.com>
2022-05-14 11:22:25 +0900

    repair discord url

--- public/assets/include/footer.php
+++ public/assets/include/footer.php
@@ -4,7 +4,7 @@
         <div class="icons">
             <a href="https://twitter.com/mlserver2408" target="_blank"><i class="fa-brands fa-twitter" style="color: #1DA1F2;"></i></a>
             <a href="https://www.youtube.com/channel/UCWSz32UUYgAzs_hVqKeqq-Q" target="_blank"><i class="fa-brands fa-youtube" style="color: #c4302b;"></i></a>
-            <a href="https://discord.gg/gaGB6Mm" target="_blank"><i class="fa-brands fa-discord" style="color: #7289da;"></i></a>
+            <a href="https://discord.mlserver.xyz" target="_blank"><i class="fa-brands fa-discord" style="color: #7289da;"></i></a>
             <a href="https://twitch.mlserver.xyz" target="_blank"><i class="fa-brands fa-twitch" style="color: #9d4bff;"></i></a>
         </div>
         <p class="foot-menu">
