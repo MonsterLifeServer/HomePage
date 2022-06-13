e9ef9185602ef2e19a5976c55cd59f807a347c5c
Monster2408 <meoto2408@gmail.com>
2022-06-13 22:53:38 +0900

    amongusのルールページ追加

---
+++ public/game/amongus.php
@@ -0,0 +1,100 @@
+<?php
+
+include('./../assets/function.php');
+$func = new HomePageFunction('./../assets/config.php', 'Among Us Craft');
+$func->setPageUrl($func->getUrl().'/game/amongus');
+$func->setDescription('企画「Among Us Craft」のルール紹介ページです。');
+
+include($func->getDiscordLibPath());
+$disLib = new DiscordLib($func->getPageUrl(), $func->getDiscordOAuth2_ID(), $func->getDiscordOAuth2_Secret());
+$disLib->initDiscordOAuth();
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+	<head>
+        <?php $func->printMetaData(); ?>
+    </head>
+    <body>
+        <?php include( $_SERVER["DOCUMENT_ROOT"] . "/assets/include/header.php"); ?>
+        <div class="wrapper">
+            
+            <div class="mainBox">
+                <div class="contents">
+                    <!-- パンくずリスト&最終更新日 -->
+                    <div class="top-label">
+                        <div class="item-left">
+                            <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+                                <li itemprop="itemListElement" itemscope
+                                    itemtype="https://schema.org/ListItem">
+                                    <a itemprop="item" href="<?php echo $func->getUrl(); ?>/">
+                                        <span itemprop="name">ホーム</span>
+                                    </a>
+                                    <meta itemprop="position" content="1" />
+                                </li>
+
+                                <li itemprop="itemListElement" itemscope
+                                    itemtype="https://schema.org/ListItem">
+                                    <a itemprop="item" href="<?php echo $func->getUrl(); ?>/game/">
+                                        <span itemprop="name">ミニゲーム企画</span>
+                                    </a>
+                                    <meta itemprop="position" content="2" />
+                                </li>
+
+                                <li itemprop="itemListElement" itemscope
+                                    itemtype="https://schema.org/ListItem">
+                                    <a itemprop="item" href="<?php echo $func->getPageUrl(); ?>">
+                                        <span itemprop="name"><?php echo $func->getTitle(); ?></span>
+                                    </a>
+                                    <meta itemprop="position" content="3" />
+                                </li>
+                            </ol>
+                        </div>
+                        <div class="item-right">
+                            <p class="fileupdate right"><span class="title">最終更新日時: </span>
+                            <?php
+                                $filetime = filemtime(basename(__FILE__));
+                                echo '<span class="date">'.date('Y/m/d ', $filetime).'</span>';
+                                echo '<span class="time">'.date('H時i分', $filetime).'</span>'; 
+                            ?></p>
+                        </div>
+                    </div>
+                    <!-- パンくずリスト&最終更新日 -->
+
+                    <h1 class="design">Among Us Craft</h1>
+
+                    <div class="flex-box2">
+
+                        <div class="sub-box">
+                            <h2>ルール</h2>
+                            <p>クルーとインポスターの二組に分かれます。</p>
+                            <p>クルーはミッションを全員で全てクリアするか，インポスターを全員追放するのが目的です。</p>
+                            <p>インポスターはクルーを全滅させるのが目的です。</p>
+                            <p>クルーはマップ内の釜戸を右クリックすることでミッションをプレイすることができます。</p>
+                        </div>
+
+                        <div class="radius-box">
+                            <h2>企画詳細</h2>
+
+                            <h3>ゲーム時間</h3>
+                            <p>未定</p><hr />
+                            <h3>最低参加人数</h3>
+                            <p>4人</p><hr />
+                            <h3>バージョン</h3>
+                            <p>1.12.2</p><hr />
+                            <h3>カテゴリ</h3>
+                            <span class="category">人狼</span>
+                            <!-- <hr />
+                            <h3>テクスチャ</h3>
+                            <p><a href="<?php echo $func->getTexture('aooni'); ?>" download>ダウンロード</a></p> -->
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <?php include( $_SERVER["DOCUMENT_ROOT"] . "/assets/include/footer.php"); ?>
+    </body>
+    <?php $func->printFootScript(); ?>
+</html>
